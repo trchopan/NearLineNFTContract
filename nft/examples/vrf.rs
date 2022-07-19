8dc36f6ec62e01d00ec8608286a0a99f4cc38d4d
Quang Tran (Chop Tr) <logan1011001@gmail.com>
2022-07-19 17:26:32 +0700

    test ed25519

---
+++ nft/examples/vrf.rs
@@ -0,0 +1,5 @@
+pub fn main() {
+    let proof_hash = "";
+    let public_key = "0303c8d894296bf7f728ff51b788c2377344c2312775c628fc5ede2ba77fc932b5";
+    let message = "";
+}
--- nft/src/lib.rs
+++ nft/src/lib.rs
@@ -18,11 +18,12 @@ NOTES:
 use near_contract_standards::non_fungible_token::metadata::{
     NFTContractMetadata, NonFungibleTokenMetadataProvider, TokenMetadata, NFT_METADATA_SPEC,
 };
-use near_contract_standards::non_fungible_token::{Token, TokenId};
 use near_contract_standards::non_fungible_token::NonFungibleToken;
+use near_contract_standards::non_fungible_token::{Token, TokenId};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::LazyOption;
-use near_sdk::json_types::ValidAccountId;
+use near_sdk::env::random_seed;
+use near_sdk::json_types::{ValidAccountId};
 use near_sdk::{
     env, near_bindgen, AccountId, BorshStorageKey, PanicOnDefault, Promise, PromiseOrValue,
 };
@@ -36,7 +37,7 @@ pub struct Contract {
     metadata: LazyOption<NFTContractMetadata>,
 }
 
-const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
+const DATA_IMAGE_SVG_CIRCLE: &str = "data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PSIwIDAgMTIyNiAxNDgxIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxwYXRoIGQ9Ik0wIDEzOTRWODdDMCA0Ni4zIDEzLjMgMTkuOCA0MCA3LjUgNjYuNy00LjggOTguNy4zIDEzNiAyM2wxMDM0IDYzNGMzNy4zIDIyLjcgNTYgNTAuMyA1NiA4M3MtMTguNyA2MC4zLTU2IDgzTDEzNiAxNDU4Yy0zNy4zIDIyLjctNjkuMyAyNy44LTk2IDE1LjUtMjYuNy0xMi4zLTQwLTM4LjgtNDAtNzkuNXoiIGZpbGw9IiNmZmYiLz48L3N2Zz4=";
 
 #[derive(BorshSerialize, BorshStorageKey)]
 enum StorageKey {
@@ -57,9 +58,9 @@ impl Contract {
             owner_id,
             NFTContractMetadata {
                 spec: NFT_METADATA_SPEC.to_string(),
-                name: "Example NEAR non-fungible token".to_string(),
-                symbol: "EXAMPLE".to_string(),
-                icon: Some(DATA_IMAGE_SVG_NEAR_ICON.to_string()),
+                name: "Squid Game theme NFT - example".to_string(),
+                symbol: "SQUIDEXAMPLE".to_string(),
+                icon: Some(DATA_IMAGE_SVG_CIRCLE.to_string()),
                 base_uri: None,
                 reference: None,
                 reference_hash: None,
@@ -98,7 +99,50 @@ impl Contract {
         receiver_id: ValidAccountId,
         token_metadata: TokenMetadata,
     ) -> Token {
-        self.tokens.mint(token_id, receiver_id, Some(token_metadata))
+        self.tokens
+            .mint(token_id, receiver_id, Some(token_metadata))
+    }
+
+    #[payable]
+    pub fn lucky_draw(&mut self, receiver_id: ValidAccountId) {
+        let rand = *random_seed().get(0).unwrap();
+        let rand_str = rand.to_string();
+        let token_metadata = TokenMetadata {
+            title: Some(format!("Test {}", rand_str)), // ex. "Arch Nemesis: Mail Carrier" or "Parcel #5055"
+            description: (Some(format!("Test description {}", rand_str))), // free-form description
+            media: Some(format!(
+                "ipfs://QmcsicJCLQcKdB2ZFMcksrVDijPE7AsAww9QgQfqf1rhRJ/{}.png",
+                rand_str
+            )), // URL to associated media, preferably to decentralized, content-addressed storage
+            media_hash: None, // Base64-encoded sha256 hash of content referenced by the `media` field. Required if `media` is included.
+            copies: Some(1), // number of copies of this set of metadata in existence when token was minted.
+            issued_at: None, // ISO 8601 datetime when token was issued or minted
+            expires_at: None, // ISO 8601 datetime when token expires
+            starts_at: None, // ISO 8601 datetime when token starts being valid
+            updated_at: None, // ISO 8601 datetime when token was last updated
+            extra: None, // anything extra the NFT wants to store on-chain. Can be stringified JSON.
+            reference: None, // URL to an off-chain JSON file with more info.
+            reference_hash: None, // Base64-encoded sha256 hash of JSON from reference field. Required if `reference` is included.
+        };
+
+        self.tokens
+            .mint(TokenId::from(rand_str), receiver_id, Some(token_metadata));
+    }
+
+    #[payable]
+    pub fn test_ed25519(&mut self, signature: String, public_key: String, message: String) {
+        use ed25519_dalek::{PublicKey, Signature};
+        use ed25519_dalek::Verifier;
+
+        let signature_bytes = hex::decode(signature).expect("Cannot decode signature");
+        let signature_ = Signature::from_bytes(&signature_bytes)
+            .expect("Cannot create signature from bytes");
+        let public_key_decode = hex::decode(public_key).expect("Cannot decode public_key");
+        let public_key =
+            PublicKey::from_bytes(&public_key_decode).expect("Cannot create public_key bytes");
+
+        let verify = public_key.verify(message.as_bytes(), &signature_);
+        assert!(verify.is_ok(), "Fail verification");
     }
 }
 
